14b2b9005fb90e15fcc01a1652aeda57cc6b2899
Arunachalam B <arun@gogosoon.com>
2022-08-25 23:45:26 +0530

    [Arunachalam] 6th commit before breaking code

--- sum.sh
+++ sum.sh
@@ -11,3 +11,4 @@ echo "Total = $total"
 # Change made on 9th commit before breaking code
 # Change made on 8th commit before breaking code
 # Change made on 7th commit before breaking code
+# Change made on 6th commit before breaking code
